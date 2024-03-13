2f62135e0e6e3940b8b58e6a955cf864b2ce99d2
luminousol <luminousolding@gmail.com>
2024-03-13 20:04:44 +0900

    "refactor : 리팩터링하기"

--- server/src/main/java/com/project/mbtitest/mbti/dto/MbtiRequestDto.java
+++ server/src/main/java/com/project/mbtitest/mbti/dto/MbtiRequestDto.java
@@ -12,27 +12,16 @@ import lombok.ToString;
 public class MbtiRequestDto {
 
     private String question1;
-
     private String question2;
-
     private String question3;
-
     private String question4;
-
     private String question5;
-
     private String question6;
-
     private String question7;
-
     private String question8;
-
     private String question9;
-
     private String question10;
-
     private String question11;
-
     private String question12;
 
 }
